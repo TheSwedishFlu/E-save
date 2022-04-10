8a4c5c5b0002b1e929e6d47a203c34f1bfd441c4
TheSwedishFlu <uffe.heden.study@gmail.com>
2022-04-10 20:15:34 +0200

    repotest

--- webcrape_el_main.py
+++ webcrape_el_main.py
@@ -1,4 +1,5 @@
 import requests
 
-web_spotpris = requests.get('https://www.elbruk.se/timpriser-se3-stockholm')
-print(web_spotpris.text)
+web_spotprice = requests.get('https://www.elbruk.se/timpriser-se3-stockholm')
+web_spotprice_raw = web_spotprice.content
+print(web_spotprice_raw)
