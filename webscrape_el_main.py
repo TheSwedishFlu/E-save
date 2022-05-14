1fb5fbf46df5e8f2874367d2f679c02cfa3fdb25
TheSwedishFlu <uffe.heden.study@gmail.com>
2022-05-14 12:04:28 +0200

    Fixed shedule every day

--- webscrape_el_main.py
+++ webscrape_el_main.py
@@ -3,6 +3,7 @@ from prompt_toolkit import print_formatted_text
 #import RPi.GPIO as GPIO
 import time
 from heapq import nsmallest
+import schedule
 
 #Import data from website
 
@@ -23,16 +24,34 @@ cheapest_hours = [i.strip(':00') for i in cheapest_hours]
 
 print(cheapest_hours)
 
-#import hour
+#import hour for time update
 now_time = time.strftime("%H")
 print(now_time)
 
-#loop for check time for on and off
-while now_time not in cheapest_hours:
-    #GPIO.output(18, GPIO.HIGH)
-    print('ON')
-else:
-    print('OFF')
+
+
+#renew fixed_scrape every day
+def scrape():
+    r = requests.get('https://www.elbruk.se/timpriser-se3-stockholm')
+    fixed_scrape = dict(zip([i[0] for i in re.findall(r"'((2[0-4]|[01]?[0-9]):([0-5]?[0-9]))'", r.text)],
+            ast.literal_eval(re.search(r"data: .*(\[.*?\])[\s\S]+(?='Idag snitt')", r.text).group(1))))
+    print(fixed_scrape)
+    return fixed_scrape
+schedule.every().day.at("00.05").do(scrape)
+
+
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
+    if now_time in cheapest_hours:
+        print(True)
+        #GPIO.output(18, GPIO.HIGH) '''put 3.3 volt current on pin 18'''
+
+    else:
+        print(False)
+        #GPIO.output(18, GPIO.LOW) '''return cullert on pin 18 to 0 volt'''
     
     
 
