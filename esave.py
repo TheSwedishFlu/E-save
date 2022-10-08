3fe1379f2a6049c2c19cc8a21185b7c1ed71c8bb
TheSwedishFlu <94489818+TheSwedishFlu@users.noreply.github.com>
2022-10-08 07:33:15 +0200

    Update esave.py
    Fix of time update schedule loop and time strip

--- esave.py
+++ esave.py
@@ -6,21 +6,30 @@ import RPi.GPIO as GPIO
 import time
 from heapq import nsmallest
 import schedule
-
 import pip._vendor.requests
+
+#Ange billigaste antal timmar per dygn att köpa el i HoursPerDay
+#Ange maxpris i öre för att ständigt köpa el i PriceLimit
+
+
+HoursPerDay = 10
+PriceLimit = 80
+
+
 
 
 ############### !!!! GPIO can only be active in code on Raspberry platform !!!!! ####################
 #GPIO.setmode(GPIO.BCM) #mode for the type of pin board you use
 #GPIO.setwarnings(False)
-#PIO.setup(18, GPIO.OUT, initial=GPIO.LOW) #set initial output on pin 18 start low volt
+#GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) #set initial output on pin 18 start low volt
 
 
 
 #get scrape from site
 def scrape():
     r = requests.get('https://www.elbruk.se/timpriser-se3-stockholm')
-    print("Daily Scrape is done")
+    print("Lowify is running - Today's price range is collected.")
+    print("Updated")
     return dict(zip([i[0] for i in re.findall(r"'((2[0-4]|[01]?[0-9]):([0-5]?[0-9]))'", r.text)],
             ast.literal_eval(re.search(r"data: .*(\[.*?\])[\s\S]+(?='Idag snitt')", r.text).group(1))))
     
@@ -30,51 +39,54 @@ fixed_scrape = scrape()
 #find the cheapest hours, set hours in nsmallest
 def cheap():
     my_dict = fixed_scrape 
-    cheapest_hour = nsmallest(8, my_dict, key=my_dict.get)
-    return [i.strip('00') for i in cheapest_hour]
+    cheapest_hour = nsmallest(HoursPerDay, my_dict, key=my_dict.get)
+    return [i.strip('0:') for i in cheapest_hour]
 
 
 #find if price is under a certain thres
 def threshold():
     price_hour = fixed_scrape
-    price_hour = {k:v for k,v in price_hour.items() if v < 50}
+    price_hour = {k:v for k,v in price_hour.items() if v < PriceLimit}
     dick = nsmallest(24, price_hour, key=price_hour.get)
-    return ([i.strip('00') for i in dick])
+    #print(dick)
+    return ([i.strip('0:') for i in dick])
 
 
 
 #schedules new scrape with  prices from site every day
-schedule.every().day.at("00:05").do(scrape)
+schedule.every().day.at("07:19").do(scrape)
 
 
 
 #run the program
 while True:
     schedule.run_pending()
-    now_time = str(time.strftime('%H')) #update time in loop
-    now_time = now_time.strip('0') + ':'
+    now_time = time.strftime("%H") #update time in loop
     
-    print(now_time)
+    if now_time != "20":
+        now_time = now_time.strip("0:")
+    
+    #print(now_time)
     cheapest_hours = cheap()
     cheap_threshold = threshold()
     print(cheapest_hours, "Are the cheapest hours today")
-    print(cheap_threshold, "Price is under 200")
+    print(cheap_threshold, "Hours when price is under" , PriceLimit, "öre/kwh. (Buying on threshold)")
     
     
-    print("Active hour right now is: ",now_time)
+    print("Active hour now is: ",now_time)
     
     if now_time in cheap_threshold:
-        print("Cheap electricity by threshold")
+        print("Buying cheap electricity by threshold")
         GPIO.output(18, GPIO.HIGH)
 
     elif now_time in cheapest_hours:
-        print("Cheapest hours of 8")
+        print("Buying cheap elecricity on low hour price right now")
         GPIO.output(18, GPIO.HIGH)
 
     else:
-        print("Right now it is damn expensive!")
+        print("Right now electricity is to expensive to buy.")
         GPIO.output(18, GPIO.LOW) 
-    time.sleep(180) #sets time between time check in seconds
+    time.sleep(30) #sets time between time check in seconds
     
 
 
@@ -87,6 +99,14 @@ while True:
 #GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) '''set initial output on pin 18 start low volt'''
 #GPIO.output(18, GPIO.HIGH) '''put 5 volt current on pin 18'''
 #GPIO.output(18, GPIO.LOW) '''return current on pin 18 to 0 volt'''
+
+
+
+
+
+
+
+
 
 
 
